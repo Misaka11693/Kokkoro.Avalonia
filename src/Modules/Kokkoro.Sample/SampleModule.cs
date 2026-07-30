70086941065839f7db857471e44591c683a36428
Misaka11639 <1647988592@qq.com>
2026-07-30 11:57:30 +0800

    q

---
+++ src/Modules/Kokkoro.Sample/SampleModule.cs
@@ -0,0 +1,64 @@
+﻿using Kokkoro.Core.Apps;
+using Kokkoro.Core.MetaModels;
+using Kokkoro.Core.Modules;
+using Kokkoro.Sample.ViewModels;
+using Kokkoro.ViewModels.Core;
+
+namespace Kokkoro.Sample;
+
+public class SampleModule : DomainModule
+{
+    public override void Initialize(IApp app)
+    {
+        app.ModuleOperations += App_ModuleOperations;
+        app.StartupCompleted += App_StartupCompleted;
+        app.Exiting += App_Exiting;
+    }
+
+    private void App_ModuleOperations(object? sender, EventArgs e)
+    {
+        MenuManager.AddModules(
+            new MenuItemMeta(" 示例模块 ", "SemiIconSetting")
+            {
+                Children = new List<MenuItemMeta>
+                {
+                    new MenuItemMeta("通知提醒","SemiIconBell",typeof(NotificationDemoViewModel)),
+                    new MenuItemMeta("Semi颜色卡","SemiIconContrast",typeof(ColorsPageViewModel))
+                }
+            }
+        );
+
+        //MenuManager.AddModules(
+        //    new MenuItemMeta("Demo 模块", "SemiIconSetting")
+        //    {
+        //        Children = new List<MenuItemMeta>
+        //        {
+        //            new MenuItemMeta("系统工具", "SemiIconSetting")
+        //            {
+        //                Children = new List<MenuItemMeta>
+        //                {
+        //                    new MenuItemMeta("通知提醒", "SemiIconBell", typeof(NotificationDemoViewModel))
+        //                }
+        //            },
+        //            new MenuItemMeta("主题设置", "SemiIconSetting")
+        //            {
+        //                Children = new List<MenuItemMeta>
+        //                {
+        //                    new MenuItemMeta("Semi颜色卡", "SemiIconContrast", typeof(ColorsPageViewModel))
+        //                }
+        //            }
+        //        }
+        //    }
+        //);
+    }
+
+    private void App_StartupCompleted(object? sender, EventArgs e)
+    {
+        Console.WriteLine("程序启动完成");
+    }
+
+    private void App_Exiting(object? sender, EventArgs e)
+    {
+        Console.WriteLine("程序退出中");
+    }
+}
---
+++ src/Modules/Kokkoro.Sample/Views/ColorsPageView.axaml.cs
@@ -0,0 +1,12 @@
+using Kokkoro.Sample.ViewModels;
+using ReactiveUI.Avalonia;
+
+namespace Kokkoro.Sample.Views;
+
+public partial class ColorsPageView : ReactiveUserControl<ColorsPageViewModel>
+{
+    public ColorsPageView()
+    {
+        InitializeComponent();
+    }
+}
